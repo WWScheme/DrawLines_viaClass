4fc8383bfc1f7fda8c429d9f88650a13ee338a40
Tianhao Wang <wanglingdebian@gmail.com>
2015-03-05 09:08:44 +0800

    God damned Tab.

--- TryClass/DrawLines.cpp
+++ TryClass/DrawLines.cpp
@@ -8,29 +8,29 @@
 // 继承通用的d3d9App类，添加了顶点指针和ID3DXLine*成员
 class DrawLines : public d3d9App {
 public:
-	DrawLines(HINSTANCE hInstance, std::string winTitle, D3DDEVTYPE devType, DWORD vertexProc);
-	~DrawLines();
+    DrawLines(HINSTANCE hInstance, std::string winTitle, D3DDEVTYPE devType, DWORD vertexProc);
+    ~DrawLines();
 
-	void DrawScene();
+    void DrawScene();
 private:
-	ID3DXLine* m_pLine;
-	D3DXVECTOR2* m_pLineArr;
+    ID3DXLine* m_pLine;
+    D3DXVECTOR2* m_pLineArr;
 };
 
 // 主函数
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, int)
 {
-	DrawLines app(hInstance, "DrawLines", D3DDEVTYPE_HAL, D3DCREATE_HARDWARE_VERTEXPROCESSING);
-	g_d3d9App = &app;
+    DrawLines app(hInstance, "DrawLines", D3DDEVTYPE_HAL, D3DCREATE_HARDWARE_VERTEXPROCESSING);
+    g_d3d9App = &app;
 
-	return g_d3d9App->MsgLoop();
+    return g_d3d9App->MsgLoop();
 }
 
 // 紧接着通用类的初始化之后进步一初始化，先创建ID3DLine对象，再指定顶点坐标
 DrawLines::DrawLines(HINSTANCE hInstance, std::string winTitle, D3DDEVTYPE devType, DWORD vertexProc)
-	: d3d9App(hInstance, winTitle, devType, vertexProc)
+    : d3d9App(hInstance, winTitle, devType, vertexProc)
 {
-	if (FAILED(D3DXCreateLine(g_pd3dDevice, &m_pLine))) {
+    if (FAILED(D3DXCreateLine(g_pd3dDevice, &m_pLine))) {
         MessageBox(0, "D3DXCreateLine FAILED", 0, 0);
         PostQuitMessage(0);
     }
@@ -44,15 +44,15 @@ DrawLines::DrawLines(HINSTANCE hInstance, std::string winTitle, D3DDEVTYPE devTy
 
 DrawLines::~DrawLines()
 {
-	if (m_pLine)
-		m_pLine->Release();
-	delete m_pLineArr;
+    if (m_pLine)
+        m_pLine->Release();
+    delete m_pLineArr;
 }
 
 // 负责绘制的成员函数总是需要自行定义
 void DrawLines::DrawScene()
 {
-	g_pd3dDevice->Clear(0, 0, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+    g_pd3dDevice->Clear(0, 0, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
 
     if (FAILED(g_pd3dDevice->BeginScene())) {
         MessageBox(0, "BeginScene FAILED", 0, 0);
